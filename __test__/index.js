3e7693d95465cdb2e47379324820e1067477851a
mister-s-tkool <mister.s.tkool@gmail.com>
2022-04-19 09:01:31 +0900

    Add docs to the test script.

--- __test__/index.js
+++ __test__/index.js
@@ -12,12 +12,40 @@ const CORE_SCRIPT_NAMES = [
   "rmmz_scenes",
 ];
 
-exports.readScripts = async (targetPluginName) => {
+/**
+ * Add the folder name of plugins to a plugin file name.
+ *
+ * @param {string} pluginName
+ * @returns {string}
+ */
+const addPluginFolderName = (pluginName) => `${PLUGIN_BASE_PATH}${pluginName}`;
+
+/**
+ * Get relative path of a script file.
+ *
+ * @param {string} fileName
+ * @returns {string}
+ */
+const getRelativePath = (fileName) =>
+  path.join(__dirname, `${SCRIPT_BASE_PATH}${fileName}.js`);
+
+/**
+ * Read an MSR plugin file.
+ *
+ * @param {string} targetPluginName
+ * @param  {...string} requiredPluginNames
+ * @returns {object} MRS
+ */
+exports.readScripts = async (targetPluginName, ...requiredPluginNames) => {
   PIXI = require("pixi.js");
-  const pluginPath = `${PLUGIN_BASE_PATH}${targetPluginName}`;
   MRS = undefined;
-  for (const fileName of CORE_SCRIPT_NAMES.concat(pluginPath)) {
-    const filePath = path.join(__dirname, `${SCRIPT_BASE_PATH}${fileName}.js`);
+  const fileNames = [
+    ...CORE_SCRIPT_NAMES,
+    ...requiredPluginNames.map(addPluginFolderName),
+    addPluginFolderName(targetPluginName),
+  ];
+  for (const fileName of fileNames) {
+    const filePath = getRelativePath(fileName);
     const script = await fs.promises.readFile(filePath, "utf-8");
     eval(script);
   }
